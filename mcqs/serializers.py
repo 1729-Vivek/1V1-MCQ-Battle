920a75339e60e316638c9b0c2f56ee032042909f
Vivek <itspersonal4vivek@gmail.com>
2024-07-14 16:30:24 +0530

    added game serializer

--- mcqs/serializers.py
+++ mcqs/serializers.py
@@ -1,5 +1,7 @@
 # serializers.py
+from django.conf import settings
 from rest_framework import serializers
+from .models import Game, MCQ 
 
 class OptionSerializer(serializers.Serializer):
     body = serializers.CharField(max_length=1000)
@@ -10,3 +12,18 @@ class MCQSerializer(serializers.Serializer):
     body = serializers.CharField(max_length=2000)
     explanation = serializers.CharField(max_length=2000)
     options = OptionSerializer(many=True)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = settings.AUTH_USER_MODEL
+        fields = ['id', 'first_name', 'last_name', 'email']
+
+
+# Game Serializer
+class GameSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+    participants = UserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Game
+        fields = ['game_id', 'owner', 'participants', 'status', 'created_at']
